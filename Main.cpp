776d9f87f503fbe8b959bea30486c72b20f21140
Martyna <martyna.plutowska@dziewczynywgrze.pl>
2023-02-27 19:34:40 +0100

    Add asteroid, now player can no longer move outside the screen, remove warnings
    [git-p4: depot-paths = "//Asteroids-depot/": change = 35]

--- Main.cpp
+++ Main.cpp
@@ -18,10 +18,15 @@ struct Vec2
 		x += b.x;
 		y += b.y;
 	}
+	void operator-=(Vec2 b) {
+		x -= b.x;
+		y -= b.y;
+	}
 	Vec2 operator*(float b) {
-		x *= b;
-		y *= b;
-		return { x, y };
+		Vec2 temp = { x, y };
+		temp.x *= b;
+		temp.y *= b;
+		return temp;
 	}
 };
 
@@ -34,6 +39,11 @@ struct SizeVec2
 //struct PositionComponent
 //{
 //	Vec2 position;
+//};
+
+//struct SizeComponent
+//{
+//	SizeVec2 size;
 //};
 
 struct TextureComponent
@@ -60,18 +70,33 @@ struct MovementComponent
 struct Player
 {
 	MovementComponent movementComp;
-	TextureComponent textureComp;
+	TextureComponent textureComp = {};
 	//PositionComponent positionComp;
+};
+
+struct Asteroid
+{
+	MovementComponent movementComp;
+	TextureComponent textureComp = {};
 };
 
 void move(MovementComponent& moveComp, float dt)
 {
 	moveComp.position += moveComp.velocity * dt;
+
+}
+
+bool is_outside_screen(const MovementComponent& moveComp, const TextureComponent& sizeComp)
+{
+	if (moveComp.position.y < 0 || (moveComp.position.y + sizeComp.size.h > SCREEN_HEIGHT) || moveComp.position.x < 0 || (moveComp.position.x + sizeComp.size.w > SCREEN_WIDTH))
+		return true;
+
+	return false;
 }
 
 void render(const TextureComponent& textureComp, const MovementComponent& moveComp)
 {
-	SDL_Rect a = { moveComp.position.x, moveComp.position.y, textureComp.size.w, textureComp.size.h };
+	SDL_Rect a = { (int)moveComp.position.x, (int)moveComp.position.y, textureComp.size.w, textureComp.size.h };
 	SDL_RenderCopy(renderer, textureComp.texture, NULL, &a);
 }
 
@@ -97,7 +122,7 @@ void handle_input(MovementComponent& moveComp)
 	}
 }
 
-SDL_Texture* load_texture(std::string path, SizeVec2 &size)
+SDL_Texture* load_texture(std::string path, SizeVec2& size)
 {
 	SDL_Texture* new_texture = NULL;
 	SDL_Surface* loaded_surface = IMG_Load(path.c_str());
@@ -106,37 +131,42 @@ SDL_Texture* load_texture(std::string path, SizeVec2 &size)
 	new_texture = SDL_CreateTextureFromSurface(renderer, loaded_surface);
 	assert(new_texture != nullptr && "Unable to create texture from image\n");
 
-	size = {loaded_surface->w, loaded_surface->h };
+	size = { loaded_surface->w, loaded_surface->h };
 
 	SDL_FreeSurface(loaded_surface);
 	loaded_surface = NULL;
-	
+
 	return new_texture;
 }
 
 int main(int argc, char* args[])
 {
-	float dt = SDL_GetTicks();
+	float dt = (float)SDL_GetTicks();
 	float lastTime = 0.f;
-	const float desiredDt = (1 / 60.f)*1000; // 60 FPS
+	const float desiredDt = (1 / 60.f) * 1000; // 60 FPS
 
 	int result = SDL_Init(SDL_INIT_VIDEO);
-	assert(result == 0 && "SDL could not initialize!\n");
+	assert(result == 0 && "SDL could not initialize!");
 
 	window = SDL_CreateWindow("Does this work", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-	assert(window != nullptr && "Window could not be created!\n");
+	assert(window != nullptr && "Window could not be created!");
 
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	assert(renderer != nullptr && "Renderer could not be created!\n");
+	assert(renderer != nullptr && "Renderer could not be created!");
 
 	result = IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG;
-	assert(result && "SDL_image could not initialize! SDL_image Error: %s\n");
+	assert(result && "SDL_image could not initialize!");
 
 	Player player;
+	Asteroid asteroid;
 	player.textureComp.texture = load_texture("ship.png", player.textureComp.size);
-	assert(player.textureComp.texture != nullptr && "Player texture could not be created!\n");
+	assert(player.textureComp.texture != nullptr && "Player texture could not be created!");
+	asteroid.textureComp.texture = load_texture("asteroids/asteroid-1.png", asteroid.textureComp.size);
+	assert(asteroid.textureComp.texture != nullptr && "Asteroid texture could not be created!");
 
-	player.movementComp.position = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, };
+	player.movementComp.position = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+	asteroid.movementComp.position = { 100, 200 };
+	asteroid.movementComp.velocity = { 0.1f, 0.1f };
 
 	bool quit = false;
 	SDL_Event event;
@@ -153,13 +183,28 @@ int main(int argc, char* args[])
 					quit = true;
 			}
 
+			/* Player */
 			handle_input(player.movementComp);
 			move(player.movementComp, dt);
+			if (is_outside_screen(player.movementComp, player.textureComp))
+				player.movementComp.position -= player.movementComp.velocity * dt;
 
+			/* Asteroid */
+			move(asteroid.movementComp, dt);
+
+			/* Render */
 			SDL_RenderClear(renderer);
+
 			render(player.textureComp, player.movementComp);
-			lastTime = SDL_GetTicks();
+
+			if (asteroid.movementComp.position.x < SCREEN_WIDTH && asteroid.movementComp.position.y < SCREEN_HEIGHT)
+				render(asteroid.textureComp, asteroid.movementComp);
+			else
+				asteroid.movementComp.position = { 0.f, (float)-asteroid.textureComp.size.h };
+
 			SDL_RenderPresent(renderer);
+
+			lastTime = (float)SDL_GetTicks();
 		}
 	}
 
