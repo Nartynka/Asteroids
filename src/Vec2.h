95d4e58c4b1cf05a9e355c2d9c1da56a9ab6f350
Bartek <bartosz.romanowski@cdprojekt.com>
2023-03-04 17:52:37 +0100

    Adding missing files
    [git-p4: depot-paths = "//Asteroids-depot/": change = 41]

---
+++ src/Vec2.h
@@ -0,0 +1,25 @@
+#pragma once
+
+struct Vec2
+{
+	float x, y;
+	void operator+=(Vec2 b) {
+		x += b.x;
+		y += b.y;
+	}
+	void operator-=(Vec2 b) {
+		x -= b.x;
+		y -= b.y;
+	}
+	Vec2 operator*(float b) {
+		Vec2 temp = { x, y };
+		temp.x *= b;
+		temp.y *= b;
+		return temp;
+	}
+};
+
+struct Size2
+{
+	int w, h;
+};
