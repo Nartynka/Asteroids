dedbc4a51c8078d1f18be6b219abbab58094f9f7
Martyna <martyna.plutowska@dziewczynywgrze.pl>
2023-04-30 19:14:11 +0100

    Visibility System, now player can't exit screen and every other entity that exits screen is destroyed
    [git-p4: depot-paths = "//Asteroids-depot/": change = 71]

--- src/Systems/MoveSystem.cpp
+++ src/Systems/MoveSystem.cpp
@@ -1,10 +1,11 @@
 #include "Systems.h"
 #include <Entity/Entity.h>
+#include <stdio.h>
 
-//@TODO: These don't belong here
 const int SPEED = 300;
 
-//@TODO: For now we can once again leave the screen
+void check_visibility(EntityAllocator* entity_alloc, Entity& entity, MovementComponent& mov_comp, float dt);
+
 void move(float dt)
 {
 	EntityAllocator* entity_alloc = EntityAllocator::Get();
@@ -18,13 +19,32 @@ void move(float dt)
 			continue;
 
 		MovementComponent mov_comp = comp_alloc->movement_components[mov_comp_id];
-		curr_entity.position.x += mov_comp.velocity.x * SPEED * dt;
-		curr_entity.position.y += mov_comp.velocity.y * SPEED * dt;
 
-		curr_entity.center += {mov_comp.velocity.x * SPEED * dt, mov_comp.velocity.y * SPEED * dt };
+		curr_entity.position += mov_comp.velocity * SPEED * dt;
+
+		check_visibility(entity_alloc, curr_entity, mov_comp, dt);
+
+		curr_entity.center = { curr_entity.position.x + (curr_entity.size.x / 2), curr_entity.position.y + (curr_entity.size.y / 2) };
 
 		if (curr_entity.is_rotating)
 			curr_entity.rotation += dt * 100;
 	}
+}
 
+void check_visibility(EntityAllocator* entity_alloc, Entity& entity, MovementComponent& mov_comp, float dt)
+{
+	if (entity.HasComponent(PLAYER))
+	{
+		Vec2 top_pos = { entity.position.x + entity.size.x, entity.position.y + entity.size.y };
+		if (entity.position.x < 0 || top_pos.x > SCREEN_WIDTH || entity.position.y < 0 || top_pos.y > SCREEN_HEIGHT)
+		{
+			printf("Player out of screen bounds!!\n");
+			entity.position -= mov_comp.velocity * SPEED * dt;
+		}
+	}
+	else if (entity.position.x < 0 || entity.position.x > SCREEN_WIDTH || entity.position.y < 0 || entity.position.y > SCREEN_HEIGHT)
+	{
+		printf("Destroying something!!\n");
+		entity_alloc->QueueDestroy(entity.id);
+	}
 }
